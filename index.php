f7b9e78ff3882db33df1ee23af6e85f25770a1ec
Devin Beeuwkes <devin@digitalnatives.nl>
2018-11-20 19:43:48 +0100

    Serve command + site index

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Documentation</title>
+</head>
+<body>
+<a href="/docs/1-algolia-and-scout.html">1. Algolia and scout</a>
+</body>
+</html>
